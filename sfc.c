f0ef3a0abc69e6b1fa96c13e65e029c82ec433e7
Ahmad Hamidullah <ahamidullah@gmail.com>
2016-05-29 07:28:33 -0400

    modified lexer

--- sfc.c
+++ sfc.c
@@ -1,17 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
-#define MAX_NUM_DIGITS 32
+enum token { num_tk, add_tk, sub_tk, eof_tk, _err_tk };
 
-char look;
+enum token look;
+char *lookstr;
+int max_lookstr_sz;
+
+int
+isdelim(char c)
+{
+	if (c == '+' || c == '-' || c == EOF || isspace(c))
+		return 1;
+	return 0;
+}
 
 void
+lookstr_append(char c)
+{
+	int new_len = strlen(lookstr)+1;
+	if (new_len >= max_lookstr_sz) {
+		max_lookstr_sz *= 2;
+		lookstr = realloc(lookstr, max_lookstr_sz);
+	}
+	lookstr[new_len-1] = c;
+	lookstr[new_len] = '\0';
+}
+
+enum token
 nexttok(void)
 {
-	while ((look = getchar()) == ' ' || look == '\t')
-	  ;
-	return;
+	char c = getc(stdin);
+	enum token ret = _err_tk;
+
+	lookstr_append(c);
+	if (c == '+')
+		return add_tk;
+	if (c == '-')
+		return sub_tk;
+	if (c == EOF)
+		return eof_tk;
+	if (isdigit(c)) {
+		ret = num_tk;
+		while (!isdelim((c = getc(stdin)))) {
+			lookstr_append(c);
+			if (!isdigit(c)) ret = _err_tk;
+		}
+		ungetc(c, stdin);
+	}
+	return ret;
 }
 
 #define debug_print(...)\
@@ -57,22 +96,10 @@ ctoi(int c)
 	return c - '0';
 }
 
-int
-num(void)
-{
-	int num = 0;
-
-	if (scanf("%d", &num) != 1)
-		expected("Integer");
-	debug_print("%d", num);
-	nexttok();
-	return num;
-}
-
 void
-term(void)
+constnum(void)
 {
-	int n = num();
+	int n = 1;
 	emitln("MOV %d %%EAX", n);
 	return;
 }
@@ -96,7 +123,7 @@ sub(void)
 void
 expr(void)
 {
-	term();
+	constnum();
 	emitln("MOV %%EAX %%EBX");
 	while (look == '+' || look == '-') {
 		if (look == '+')
@@ -111,6 +138,16 @@ expr(void)
 int
 main(int argc, char **argv)
 {
+	/* 
+	expr ->  const expr_prime
+	expr_prime -> + expr expr_prime
+		|        - expr expr_prime
+		|       eps
+	*/
+	lookstr = malloc(1);
+	lookstr[0] = '\0';
+	max_lookstr_sz = 1;
+	look = nexttok();
 	while (1) {
 		expr();
 	}
